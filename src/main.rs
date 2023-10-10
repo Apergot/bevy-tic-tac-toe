408fd9d4a3d89bc4290b6974c7727761a02df2d2
Alejandro Perdomo <apergot95@gmail.com>
2023-10-10 13:10:07 +0100

    add method to check if game should continue

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod turns;
+
 use bevy::prelude::*;
 
 fn main() {
--- src/turns.rs
+++ src/turns.rs
@@ -1,6 +1,7 @@
 const MAX_NUMBER_OF_TURNS: i32 = 9;
 const ODD_PLAYER_CHARACTER: char = 'X';
 const EVEN_PLAYER_CHARACTER: char = 'O';
+const OUT_OF_RANGE_ERROR_MESSAGE: &'static str = "Turn out of range";
 
 fn current_turn_is_odd(turn: i32) -> bool {
     turn%2 != 0
@@ -10,10 +11,75 @@ fn current_turn_is_even(turn: i32) -> bool {
     turn%2 == 0
 }
 
-pub fn define_next_turn(current_turn: &i32) {
+pub fn define_next_turn(
+    current_turn: &mut i32, 
+    game_has_finished: &mut bool
+) -> Result<(), &'static str> {
 
+    if *current_turn <= 0 || *current_turn > MAX_NUMBER_OF_TURNS {
+        return Err(OUT_OF_RANGE_ERROR_MESSAGE)
+    }
+
+    if *current_turn == MAX_NUMBER_OF_TURNS {
+        *game_has_finished = true;
+    } else {
+        *current_turn += 1;
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
 mod turns {
+    use super::*;
+
+    #[test]
+    fn define_next_turn_for_starting_turn() {
+        let mut current_turn: i32 = 1;
+        let mut game_has_finished: bool = false;
+
+        assert_eq!(define_next_turn(&mut current_turn, &mut game_has_finished), Ok(()));
+
+        assert!(!game_has_finished);
+        assert_eq!(current_turn, 2);
+    }
+
+    #[test]
+    fn define_next_turn_when_game_should_finish() {
+        let mut current_turn: i32 = 9;
+        let mut game_has_finished: bool = false;
+
+        assert_eq!(define_next_turn(&mut current_turn, &mut game_has_finished), Ok(()));
+
+        assert_eq!(current_turn, 9);
+        assert!(game_has_finished);
+    }
+
+    #[test]
+    fn define_next_turn_when_turn_is_out_of_range_lower_limit() {
+        let mut current_turn: i32 = 0;
+        let mut game_has_finished: bool = false;
+
+        assert_eq!(
+            define_next_turn(&mut current_turn, &mut game_has_finished), 
+            Err(OUT_OF_RANGE_ERROR_MESSAGE)
+        );
+
+        assert_eq!(current_turn, 0);
+        assert!(!game_has_finished);
+    }
+
+    #[test]
+    fn define_next_turn_when_turn_is_out_of_range_higher_limit() {
+        let mut current_turn: i32 = 10;
+        let mut game_has_finished: bool = false;
+
+        assert_eq!(
+            define_next_turn(&mut current_turn, &mut game_has_finished),
+            Err(OUT_OF_RANGE_ERROR_MESSAGE)
+        );
+
+        assert_eq!(current_turn, 10);
+        assert!(!game_has_finished);
+    }
 }
