4b4da4053535c739664b5b0b6cf3cd56c96a2e90
Alejandro Perdomo <apergot95@gmail.com>
2023-10-11 12:28:38 +0100

    added method to check if last move was a winning turn

--- src/turns/errors.rs
+++ src/turns/errors.rs
@@ -1,3 +1,4 @@
 pub const OUT_OF_RANGE_ERROR_MESSAGE: &'static str = "Turn out of range";
 pub const INVALID_COORDINATES_PROVIDED: &'static str = "Invalid coordinates provided";
 pub const CELL_ALREADY_OCCUPIED: &'static str = "Given cell coordinates are already occupied";
+pub const FOUND_INVALID_CHARACTER_AT_LAST_MOVE_COORDINATES: &'static str = "Last move coordinates contain invalid character";
--- src/turns/mod.rs
+++ src/turns/mod.rs
@@ -9,11 +9,11 @@ pub fn fill_game_board_coordinates_based_on_turn(
     let(row, col) = coordinates;
 
     if row < 0 || row > 2 || col < 0 || col > 2 {
-        return Err(errors::INVALID_COORDINATES_PROVIDED)
+        return Err(errors::INVALID_COORDINATES_PROVIDED);
     }
 
     if game_board[row as usize][col as usize] != EMPTY_CELL_CHARACTER {
-        return Err(errors::CELL_ALREADY_OCCUPIED)
+        return Err(errors::CELL_ALREADY_OCCUPIED);
     }
     
     let character = if current_turn % 2 == 0 { EVEN_TURN_CHARACTER } else { ODD_TURN_CHARACTER };
@@ -40,6 +40,146 @@ pub fn define_next_turn(
     }
 }
 
+pub fn check_win_on_last_move(
+    game_board: &[[char; 3]; 3],
+    last_move: (i32, i32)
+) -> Result<bool, &'static str> {
+    let row: usize = last_move.0 as usize;
+    let col: usize = last_move.1 as usize;
+
+    if row >= 3 || col >= 3 {
+        return Err(errors::INVALID_COORDINATES_PROVIDED);
+    }
+
+    let player_symbol: char = game_board[row][col];
+
+    if player_symbol != EVEN_TURN_CHARACTER && player_symbol != ODD_TURN_CHARACTER {
+        return Err(errors::FOUND_INVALID_CHARACTER_AT_LAST_MOVE_COORDINATES)
+    }
+
+    if (0..3).all(|i| game_board[row][i] == player_symbol) {
+        return Ok(true);
+    }
+
+    if (0..3).all(|i| game_board[i][col] == player_symbol) {
+        return Ok(true);
+    }
+
+    if row == col && (0..3).all(|i| game_board[i][i] == player_symbol) {
+        return Ok(true);
+    }
+
+    if row + col == 2 && (0..3).all(|i| game_board[i][2 - i] == player_symbol) {
+        return Ok(true);
+    }
+
+    Ok(false)
+}
+
+#[cfg(test)]
+mod check_win_on_last_move {
+    use super::*;
+
+    #[test]
+    fn first_horizontal_row_check_should_return_true() {
+        let game_board: [[char; 3]; 3] = [
+            ['X', 'X', 'X'],
+            ['O', 'O', '_'],
+            ['_', 'X', 'O'],
+        ];
+        let last_move = (0, 2);
+
+        let result = check_win_on_last_move(&game_board, last_move);
+
+        assert_eq!(result, Ok(true));
+    }
+
+    #[test]
+    fn first_vertical_column_check_should_return_true() {
+        let game_board: [[char; 3]; 3] = [
+            ['X', 'O', 'X'],
+            ['X', 'O', '_'],
+            ['X', 'X', 'O'],
+        ];
+        let last_move = (2, 0);
+
+        let result = check_win_on_last_move(&game_board, last_move);
+
+        assert_eq!(result, Ok(true));
+    }
+
+    #[test]
+    fn primary_diagonal_check_should_return_true() {
+        let game_board: [[char; 3]; 3] = [
+            ['O', 'X', 'X'],
+            ['X', 'O', '_'],
+            ['X', 'O', 'O'],
+        ];
+        let last_move = (2, 2);
+
+        let result = check_win_on_last_move(&game_board, last_move);
+
+        assert_eq!(result, Ok(true));
+    }
+
+    #[test]
+    fn secondary_diagonal_check_should_return_true() {
+        let game_board: [[char; 3]; 3] = [
+            ['O', 'X', 'X'],
+            ['X', 'X', '_'],
+            ['X', 'O', 'O'],
+        ];
+        let last_move = (2, 0);
+
+        let result = check_win_on_last_move(&game_board, last_move);
+
+        assert_eq!(result, Ok(true));
+    }
+
+    #[test]
+    fn last_move_not_winning_turn_should_return_false() {
+        let game_board: [[char; 3]; 3] = [
+            ['X', 'O', 'X'],
+            ['_', 'O', '_'],
+            ['X', '_', 'O'],
+        ];
+        let last_move = (1, 1);
+
+        let result = check_win_on_last_move(&game_board, last_move);
+
+        assert_eq!(result, Ok(false));
+    }
+
+    #[test]
+    fn given_invalid_coordinates_should_return_error() {
+        let game_board: [[char; 3]; 3] = [
+            ['X', 'O', 'X'],
+            ['_', 'O', '_'],
+            ['X', '_', 'O'],
+        ];
+        let last_move = (3, 1);
+
+        let result = check_win_on_last_move(&game_board, last_move);
+
+        assert_eq!(result, Err(errors::INVALID_COORDINATES_PROVIDED));
+    }
+
+    #[test]
+    fn invalid_character_at_provided_coordinates_should_return_error() {
+        let game_board: [[char; 3]; 3] = [
+            ['X', 'O', 'A'],
+            ['_', 'O', '_'],
+            ['X', '_', 'O'],
+        ];
+        let last_move = (0, 2);
+
+        let result = check_win_on_last_move(&game_board, last_move);
+
+        assert_eq!(result, Err(errors::FOUND_INVALID_CHARACTER_AT_LAST_MOVE_COORDINATES));
+    }
+}
+
+#[cfg(test)]
 mod fill_game_board_coordinates_based_on_turn {
     use super::*;
     #[test]
