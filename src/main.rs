1af6ecbf4c0ced1380b024cbe4859285d131aa1c
Alejandro Perdomo <apergot95@gmail.com>
2023-10-12 22:16:51 +0100

    removed cli version and started played around with bevy

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,44 @@
 mod turns;
 mod constants;
-use std::io;
+use bevy::prelude::*;
 
 fn main() {
+    
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::WHITE))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup (
+    mut commands: Commands
+){
     let mut starting_turn: i32 = 1;
     let mut game_has_finished: bool = false;
     let mut game_board: [[char; 3]; 3] = [[constants::EMPTY_CELL_CHARACTER; 3]; 3];
 
-    while !game_has_finished {
-        let coordinates = get_coordinates_from_user_input();
-        match turns::define_next_turn(&mut starting_turn, &mut game_has_finished) {
-            Ok(()) => {
-                match turns::fill_game_board_coordinates_based_on_turn(starting_turn, &mut game_board, coordinates) {
-                    Ok(()) => {
-                        match turns::check_win_on_last_move(&game_board, coordinates) {
-                            Ok(true) => {
-                                println!("Player with symbol {} has won!", game_board[coordinates.0 as usize][coordinates.1 as usize]);
-                                print_game_board_to_console(game_board);
-                                break;
-                            },
-                            Ok(false) => {
-                                print_game_board_to_console(game_board);
-                                println!("Starting next turn!")
-                            },
-                            Err(err_msg) => {
-                                println!("Error: {}", err_msg)
-                            }
-                        }
-                    },
-                    Err(err_msg) => {
-                        println!("Error: {}", err_msg)
-                    }
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(
+        TextBundle::from_sections([
+            TextSection::new(
+                "Bevy Tic-Tac-Toe",
+                TextStyle {
+                    font_size: 40.0, 
+                    color: Color::BLACK,
+                    ..default()
                 }
-            },
-            Err(err_msg) => {
-                println!("Error: {}", err_msg);
-                println!("Game has finished due to max number of turns reached");
-                break;
+            )
+        ])
+        .with_text_alignment(TextAlignment::Center)
+        .with_style(
+            Style {
+                position_type: PositionType::Absolute,
+                top: Val::Px(5.0),
+                right: Val::Px(10.0),
+                ..default()
             }
-        }
-    }
-}
-
-fn get_coordinates_from_user_input() -> (i32, i32) {
-    let mut input = String::new();
-
-    println!("Provide row:");
-    io::stdin().read_line(&mut input).expect("Error reading input");
-    let row: i32 = input.trim().parse().expect("Unable to retrieve row value");
-
-    input.clear();
-
-    println!("Provide col:");
-    io::stdin().read_line(&mut input).expect("Error reading input");
-    let col: i32 = input.trim().parse().expect("Unable to retrieve col value");
-
-    (row, col)
-}
-
-fn print_game_board_to_console(game_board: [[char; 3]; 3]) {
-    for row in &game_board {
-        for &cell in row {
-            print!("{} ", cell);
-        }
-        println!();
-    }
+        )
+    );
 }
